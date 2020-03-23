b0a580be8a3e22a23157a72b411923ca83eae3cc
Guillermo <gvm104@gmail.com>
2020-03-23 21:58:09 +0100

    Some Changes
    + clean up

--- nasa.py
+++ nasa.py
@@ -1,16 +1,20 @@
+# Created for fun by Guillermo Ventura 
 
-# ISS api
 import json
 import requests
 
 import time
 import datetime
-import icalendar
 
+#pimport icalendar
+
+# debugging
 from termcolor import colored
+import sys
 
 # user location
-#from geopy.distance import vincenty
+import geopy.distance #from geopy.distance import vincenty
+import geocoder
 
 user_lat = 0
 user_lon = 0 
@@ -30,72 +34,48 @@ def iss_data(data):
 	return iss_position['latitude'], iss_position['longitude']
 
 
+
 def user_position():
-	print(" * * * * * * * * * USER * * * * * * * * * * *")
-	send_url = 'http://freegeoip.net/json'
-	r = requests.get(send_url)
-	print(r['message'])
-	sys.exit()
-	location = json.loads(r.text)
+	print(" * * * * * * * * * USER * * * * * * * * * * * ")
+	g = geocoder.ip('me')
+	print(g.latlng)
+	lat=g.latlng[0]
+	lng=g.latlng[1]
 
-	print(colored('Latitude:','blue'))
-	print(location['latitude'])
-	print(colored('Longitude:','magenta'))
-	print(location['longitude'])
-
-	return location['latitude'],location['longitude']
+	return lat, lng
 
 
 def calculate_distance(iss_lat, iss_lon, user_lat, user_lon):
 
 	print(" * * * * * * * * DISTANCE * * * * * * * * * * ")
-	distance = vincenty((iss_lat,iss_lon), (user_lat, user_lon)).meters
-	print(distance, 'meters')
-
-	response = requests.request('GET','http://api.open-notify.org/iss-pass.json?lat='+str(user_lat)+'&lon='+str(user_lon)+'&n=5')
-	data = json.loads(response.text)
-
-	if data['message'] == 'success':
-		print(" success")
-		next_times =  data['response']
-		for time in next_times:
-			print("duration")
-			print(time['duration'])
-			print("time")
-			#print time['risetime']
-			print(datetime.datetime.fromtimestamp(time['risetime']).strftime('%Y-%m-%d %H:%M:%S'))
-
+	distance = geopy.distance.geodesic((iss_lat,iss_lon), (user_lat, user_lon)).meters
+	print(distance, 'kilometers')
 
 	return distance # new distance not global
 
 
 
 def init():
+	global user_lat,user_lon
 	response = requests.request('GET', 'http://api.open-notify.org/iss-now.json')
-
-
 	data = json.loads(response.text)
-
-	#print(data)
-
 
 	if data['message'] == 'success':
 		print(colored('SUCCESS','green'))
 		iss_lat, iss_lon   = iss_data(data)
+		if user_lat == 0 and user_lon == 0:
+			user_lat, user_lon = user_position()
 
-	global user_lat, user_lon 
-
-	if user_lat == 0 or  user_lon == 0:
-		user_lat, user_lon = user_position()
-
-	distance_new = calculate_distance(iss_lat, iss_lon, user_lat,user_lon)
-	distance_arr.append(distance_new)
+		distance_new = calculate_distance(iss_lat, iss_lon, user_lat,user_lon)
+		distance_arr.append(distance_new)	
+	
+	else:
+		print(colored('Something went wrong requesting ISS data:','red'))
+		print(data)
 
 
 while True:
 	init()
-	#time.sleep(5)
-	break
-
-# NASA-API
-#api_key = "3f5LI3hMi848kx2xL0cWcvQOPasG9kvgeCSoHCPL"
+	time.sleep(5)
+	print(' - * - * - * - * - * - * - * - * - * - * - * - * - * - * ')
+	#break
